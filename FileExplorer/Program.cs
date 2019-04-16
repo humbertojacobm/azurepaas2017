7d3f5d10495116de0fd67340f1f602eb8343ea21
Humberto Jacob Mori Guerra <hmori@belatrix.com>
2019-04-16 18:11:36 -0500

    module 03 video 13 setting file storage application

---
+++ FileExplorer/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.Azure;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.File;
+
+namespace FileExplorer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CloudStorageAccount cuentaAlmacenamiento =
+                CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("cadenaConexion"));
+
+        }
+    }
+}
