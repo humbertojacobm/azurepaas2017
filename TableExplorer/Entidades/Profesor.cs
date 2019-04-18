d023cfefefca0229c1db0b4887b1cf269eb677ca
Humberto Jacob Mori Guerra <hmori@belatrix.com>
2019-04-18 17:12:39 -0500

    modulo 03 video 17 agregar entidades a una tabla

---
+++ TableExplorer/Entidades/Profesor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace TableExplorer.Entidades
+{
+    public class Profesor: TableEntity
+    {
+        public Profesor(string identificador, string categoria)
+        {
+            this.PartitionKey = identificador;
+            this.RowKey = categoria;
+        }
+        public Profesor()
+        {
+
+        }
+
+        public string NombreProfesor { get; set; }
+        public string NombreAsignatura { get; set; }
+
+
+    }
+}
--- TableExplorer/Program.cs
+++ TableExplorer/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.WindowsAzure;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
+using TableExplorer.Entidades;
 
 namespace TableExplorer
 {
@@ -14,15 +15,35 @@ namespace TableExplorer
             CloudTableClient clienteTablas = cuentaAlmacenamiento.CreateCloudTableClient();
             CloudTable tabla = clienteTablas.GetTableReference("clases");
 
-            tabla.CreateIfNotExists();
+            //tabla.CreateIfNotExists();
 
-            var nombreTablas = clienteTablas.ListTables();
+            //var nombreTablas = clienteTablas.ListTables();
 
-            foreach (CloudTable item in nombreTablas)
-            {
-                System.Console.WriteLine(item.Name);
-            }
+            //foreach (CloudTable item in nombreTablas)
+            //{
+            //    System.Console.WriteLine(item.Name);
+            //}
 
+            Profesor profeUno = new Profesor("001", "Profesores");
+
+            profeUno.NombreProfesor = "Ricardo Celis";
+            profeUno.NombreAsignatura = "Microcontraladores";
+
+            Profesor profeDos = new Profesor("002", "Profesores");
+
+            profeDos.NombreProfesor = "Carlos Paredes";
+            profeDos.NombreAsignatura = "Diseño audiovisual";
+
+            TableOperation insertarProfeUno = TableOperation.Insert(profeUno);
+
+            TableOperation insertarProfeDos = TableOperation.Insert(profeDos);
+
+            tabla.Execute(insertarProfeUno);
+
+            tabla.Execute(insertarProfeDos);
+
+            System.Console.WriteLine("Tus entidades han sido insertadas");
+           
             System.Console.ReadLine();
         }
     }
