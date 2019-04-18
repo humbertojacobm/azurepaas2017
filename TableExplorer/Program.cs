dc073986eab10b4c441eecfb941ce75406872752
Humberto Jacob Mori Guerra <hmori@belatrix.com>
2019-04-18 11:43:51 -0500

    module 02 video 16 sdk net para almacenamiento de tabla

---
+++ TableExplorer/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace TableExplorer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CloudStorageAccount cuentaAlmacenamiento =
+                CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("cadenaconexion"));
+
+        }
+    }
+}
