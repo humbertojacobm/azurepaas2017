27b1a601c335cb3e6142b656642dc517e9ddc632
Humberto Jacob Mori Guerra <hmori@belatrix.com>
2019-04-22 17:41:10 -0500

    module 03 v25 create queues

--- QueueExplorer/Program.cs
+++ QueueExplorer/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.WindowsAzure;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Queue;
+using System;
 
 namespace QueueExplorer
 {
@@ -13,6 +14,17 @@ namespace QueueExplorer
                 CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("cadenaconexion"));
 
             CloudQueueClient clienteColas = miCuenta.CreateCloudQueueClient();
+
+            CloudQueue queue = clienteColas.GetQueueReference("mifiladeprocesos");
+
+            queue.CreateIfNotExists();
+
+            for (int i = 0; i < 500; i++)
+            {
+                CloudQueueMessage mensaje = new CloudQueueMessage(string.Format("Operation: {0}", i));
+                queue.AddMessage(mensaje);
+                Console.WriteLine(i.ToString()+"tu nuevo mensaje a sido publicado ");
+            }
         }
     }
 }
