157d37451ef93982c8786f81fb8225a3fcf959cc
Humberto Jacob Mori Guerra <hmori@belatrix.com>
2019-04-22 18:03:06 -0500

    m2 v25 writing code to get message from queue

---
+++ QueueHandler/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System.IO;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+
+namespace QueueHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CloudStorageAccount miCuenta =
+                CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("cadenaconexion"));
+
+            CloudQueueClient clienteColas = miCuenta.CreateCloudQueueClient();
+
+            CloudQueue queue = clienteColas.GetQueueReference("mifiladeprocesos");
+
+            CloudQueueMessage peekedMessage = queue.PeekMessage();
+
+            foreach (CloudQueueMessage item in queue.GetMessages(250))
+            {
+                string rutaArchivo = string.Format(@"c:\Tempr\log{0}.txt", item.Id);
+                TextWriter archivoTemp = File.CreateText(rutaArchivo);
+                var mensaje = queue.GetMessage().AsString;
+                archivoTemp.WriteLine(mensaje);
+                Console.WriteLine("Archivo creado");
+                archivoTemp.Close();
+            }
+
+        }
+    }
+}
