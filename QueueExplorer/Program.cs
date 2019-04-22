0112ba6581e3c0e25723e45f7b1607b22341de6a
Humberto Jacob Mori Guerra <hmori@belatrix.com>
2019-04-22 16:59:19 -0500

    module 02 video 23 crear cliente para almacenamiento de colas

---
+++ QueueExplorer/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace QueueExplorer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CloudStorageAccount miCuenta =
+                CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("cadenaconexion"));
+
+            CloudQueueClient clienteColas = miCuenta.CreateCloudQueueClient();
+        }
+    }
+}
